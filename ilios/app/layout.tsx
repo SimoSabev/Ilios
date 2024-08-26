eec1795528a06028ca5dd8f17a7ae78c863f2f2a
SimoSabev <sabevsimeon08@gmail.com>
2024-08-26 16:55:29 +0300

    fix evrt

--- ilios/app/layout.tsx
+++ ilios/app/layout.tsx
@@ -1,28 +1,38 @@
-  import type { Metadata } from "next";
-  import { Raleway } from "next/font/google";
-  import "./globals.css";
-  import logo from "../public/ico.jpg"
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+import favicon from "../public/ico.jpg";
+import ogImage from "../public/ico.jpg"; // Path to your Open Graph image
 
-  const raleway = Raleway({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
-  export const metadata: Metadata = {
+export const metadata: Metadata = {
+  title: "ILIOS",
+  description: "Interior Project Management",
+  openGraph: {
     title: "ILIOS",
     description: "Interior Project Management",
-  };
+    images: [ogImage.src],
+    url: "https://www.iliosdecor.com",
+  },
+};
 
-  export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
       <head>
-        <link rel="icon" href={logo.src} />
+        <link rel="icon" href={favicon.src} />
+        <meta property="og:image" content={ogImage.src} />
+        <meta property="og:title" content="ILIOS - Interior Project Management" />
+        <meta property="og:description" content="Description of your site" />
       </head>
-        <body className={` ${raleway.className} bg-background text-foreground overflow-x-hidden  `}>
-          {children}
-        </body>
-      </html>
-    );
-  }
+      <body className={`${raleway.className} bg-background text-foreground overflow-x-hidden`}>
+        {children}
+      </body>
+    </html>
+  );
+}
